b48976feeaf4839a04ae9a01ab6ca15486acb758
Naveen Manjappa <NManjappa@manh.com>
2024-10-20 07:01:49 +0100

    11 Todo list page

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,5 +1,20 @@
 const todoList = [];
 
+renderTodoList();
+
+function renderTodoList() {
+    let todoListHtml = '';
+
+    for(let i = 0; i < todoList.length; i++){
+      const todo = todoList[i];
+      const html = `<p>${todo}</p>`;
+      todoListHtml += html;
+    }
+
+    document.querySelector('.js-todo-list')
+    .innerHTML = todoListHtml;
+}
+
 function addTodo() {
   const inputEl = document.querySelector('.js-input');
 
@@ -8,4 +23,6 @@ function addTodo() {
   todoList.push(name); 
   console.log(todoList);
   inputEl.value = '';
+
+  renderTodoList();
 }
